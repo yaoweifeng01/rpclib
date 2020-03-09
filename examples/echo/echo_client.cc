d198ff5fd11cbdf8f58eaa41261e638eef933dc3
yaoweifeng01 <yaoweifeng01@baidu.com>
2020-03-09 11:56:41 +0800

    fix unit

--- examples/echo/echo_client.cc
+++ examples/echo/echo_client.cc
@@ -27,23 +27,31 @@ class Cyber_ge_adapter
             }
         }
 
+        // millisecond:毫秒 1s = 1000毫秒
+        // microseconds:微秒 1s = 1000 000 微妙
         void ProcLocalization()
         {
             while (true)
             {
-                try {
+                //try
+                {
+                    auto start = std::chrono::steady_clock::now();
                     //ge_server::ByteResponse result(client_->call("getlocalization").as<ge_server::ByteResponse>());
                     auto result = vClient_[0]->call("add1", 3,4).as<int>();
+                    //std::this_thread::sleep_for(std::chrono::milliseconds (100));
+                    auto end = std::chrono::steady_clock::now();
+
+                    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << "ms \n";
                     //std::cout << "result:" << result << std::endl;
                 }
-                catch (const std::future_error& e)
-                {
-                    std::cout << "add catch error code:" << e.code() << "\nMessage: " << e.what()<< std::endl;
-                }
-                catch (...)
-                {
-                    std::cout << "ProcLocalization all" << std::endl;
-                }
+                // catch (const std::future_error& e)
+                // {
+                //     std::cout << "add catch error code:" << e.code() << "\nMessage: " << e.what()<< std::endl;
+                // }
+                // catch (...)
+                // {
+                //     std::cout << "ProcLocalization all" << std::endl;
+                // }
                 // std::cout << "ProcLocalization > " <<  result.byte_data_uint8.size() << std::endl;
                 // if (result.byte_data_uint8.empty())
                 // {
@@ -274,21 +282,21 @@ class Cyber_ge_adapter
         {
             std::cout << "begin listen ......." << std::endl;
             std::thread t_localizaiton(&Cyber_ge_adapter::ProcLocalization, this);
-            std::thread t_PlanningMessage(&Cyber_ge_adapter::ProcPlanningMessage, this);
-            std::thread t_PerceptionObstacles(&Cyber_ge_adapter::ProcPerceptionObstacles, this);
-            std::thread t_PerceptionObstacles_gt(&Cyber_ge_adapter::ProcPerceptionObstacles_gt, this);
-            std::thread t_Lidar(&Cyber_ge_adapter::PorcLidar, this);
-            std::thread t_Camera(&Cyber_ge_adapter::PorcCamera, this);
-            std::thread t_TF(&Cyber_ge_adapter::ProcTF, this);
-            std::thread t_TrafficLightDetection(&Cyber_ge_adapter::ProcTrafficLightDetection, this);
+            // std::thread t_PlanningMessage(&Cyber_ge_adapter::ProcPlanningMessage, this);
+            // std::thread t_PerceptionObstacles(&Cyber_ge_adapter::ProcPerceptionObstacles, this);
+            // std::thread t_PerceptionObstacles_gt(&Cyber_ge_adapter::ProcPerceptionObstacles_gt, this);
+            // std::thread t_Lidar(&Cyber_ge_adapter::PorcLidar, this);
+            // std::thread t_Camera(&Cyber_ge_adapter::PorcCamera, this);
+            // std::thread t_TF(&Cyber_ge_adapter::ProcTF, this);
+            // std::thread t_TrafficLightDetection(&Cyber_ge_adapter::ProcTrafficLightDetection, this);
             t_localizaiton.join();
-            t_PlanningMessage.join();
-            t_PerceptionObstacles.join();
-            t_PerceptionObstacles_gt.join();
-            t_Lidar.join();
-            t_Camera.join();
-            t_TF.join();
-            t_TrafficLightDetection.join();
+            // t_PlanningMessage.join();
+            // t_PerceptionObstacles.join();
+            // t_PerceptionObstacles_gt.join();
+            // t_Lidar.join();
+            // t_Camera.join();
+            // t_TF.join();
+            // t_TrafficLightDetection.join();
         }
     private:
         std::shared_ptr<rpc::client> client_ = nullptr;
